fcce91c0a390a42f06e597929037a3f6ef96a1c1
Salvador HM <salvadorhm@gmail.com>
2022-01-20 18:43:31 +0000

    formulario funcionando

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import web
 urls = (
     '/', 'Index'
 )
+
 app = web.application(urls, globals())
+render = web.template.render('views')
 
 class Index:
     def GET(self):
-        pagina = "<html><body>hola</body></html>"
-        return pagina
+        return render.index()
+
+    def POST(self):
+        formulario = web.input()
+        return formulario.nombre
 
 if __name__ == "__main__":
     app.run()
